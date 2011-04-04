0f6eb2b1c31525b77ddbb6ecd353a51614dff563
Gustavo Brunoro <brunoro@devio.us>
2011-04-04 13:16:51 -0300

    added tree and node headers

---
+++ src/tree.h
@@ -0,0 +1,19 @@
+#include "node.h"
+
+#include <QString>
+#include <QVector>
+#include <QPointF>
+
+class Tree:
+    public:
+        Tree(QString line, QObject *parent);
+        ~Tree();
+
+    private:
+        int id;
+        Node *root;
+        QVector<Node*> index;
+        QVector<Tree*> parents;
+        QVector<Tree*> offspring;
+
+        void draw(QPointF coord, int radius);
